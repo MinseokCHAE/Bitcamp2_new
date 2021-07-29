f3867f45a889da445f74c206927b6bf2bbfab48e
MinseokCHAE <minseok6739@naver.com>
2021-07-29 14:34:57 +0900

    update_0729_14:34

---
+++ project/soccer_prediction/test.py
@@ -0,0 +1,19 @@
+import time
+import datetime
+import sqlite3
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.metrics import r2_score, mean_squared_error
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.layers import Dense, Input, Flatten, Dropout
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+
+#1. Data Preprocessing
+# read_sql
+path = sqlite3.connect('../_data/soccer_prediction/database.sqlite')   # 데이터파일 경로설정
+df_players = pd.read_sql_query('SELECT * FROM Player', path)
+df_stats = pd.read_sql_query('SELECT * FROM Player_Attributes', path)
+
