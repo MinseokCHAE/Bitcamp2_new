0823b248bee8fdb3a1e974200bb594778d349ff0
MinseokCHAE <minseok6739@naver.com>
2021-08-10 13:09:46 +0900

    update_0810_13:09

--- ml/ml02_and.py
+++ ml/ml02_and.py
@@ -3,7 +3,7 @@ from sklearn.svm import LinearSVC
 from sklearn.metrics import accuracy_score
 
 
-#1. Data Preprocessing
+#1. Data 
 x = [[0,0], [0,1], [1,0], [1,1]]
 y = [0,0,0,1]
 
@@ -16,3 +16,4 @@ model.fit(x,y)
 #4. Evaluating
 score = model.score(x,y)
 print('score = ', score)
+# score =  1.0
