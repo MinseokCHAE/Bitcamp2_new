21a8f20094726873e3603ff342ebbdf014c25e3f
MinseokCHAE <minseok6739@naver.com>
2021-08-18 15:01:01 +0900

    update_0818_15:00

--- ml/ml29_wine_quality.py
+++ ml/ml29_wine_quality.py
@@ -35,11 +35,11 @@ y = datasets[:, 11]
 newlist = []
 for i in list(y):
     if i<=4 :
-        newlist +=[0]
+        newlist += [0]
     elif i<=7 :
-        newlist +=[1]
+        newlist += [1]
     else:
-        newlist +=[2]
+        newlist += [2]
 y = np.array(newlist)
 # print(y.shape)  # (4898,)
 
@@ -51,5 +51,7 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_
 model = XGBClassifier()
 model.fit(x_train, y_train)
 score = model.score(x_test, y_test)
-# print('score = ', score)     # score =  0.6653061224489796 -> score =  0.9551020408163265
+# print('score = ', score)     
+# # score =  0.6653061224489796 
+# -> score =  0.9551020408163265
 
