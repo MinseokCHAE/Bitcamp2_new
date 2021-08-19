304e701c9b31014f1c5402645b77667dabcd139c
MinseokCHAE <minseok6739@naver.com>
2021-08-19 18:01:10 +0900

    update_0819_18:00

--- _test/_test01.py
+++ _test/_test01.py
@@ -1,7 +1,26 @@
 
-print('hello')
+tuple_example = (['Mourinho', 'a'], ['Herrera', 'b'])
 
-a = 1
-b = 2
-c = a + b
-print(c)
+# print(tuple_example[0][1]) # a
+tuple_example[0][1] = 'c'
+# print(tuple_example[0][1]) # c
+
+# print(tuple_example[0]) # ['Mourinho', 'c']
+# tuple_example[0] = ['McTominay', 'd'] 
+# TypeError: 'tuple' object does not support item assignment
+
+
+
+dict_example = {'Son' : 'TotH', 'DeBruyne' : 'ManC', 'Pogba' : 'ManU', 'Messi' : 'PSG'}
+
+print(dict_example['Messi']) # PSG
+dict_example['Messi'] = 'ManC'
+print(dict_example['Messi']) # ManC
+
+print(dict_example.items())
+# [('Son', 'TotH'), ('DeBruyne', 'ManC'), ('Pogba', 'ManU'), ('Messi', 'ManC')]
+dict_example['Chae'] = 'ManU'
+print(dict_example.items())
+# [('Son', 'TotH'), ('DeBruyne', 'ManC'), ('Pogba', 'ManU'), ('Messi', 'ManC'), ('Chae', 'ManU')]
+
+
