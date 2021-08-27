15b35a74c04d6530b06b956e8b48d0587daa7c5e
MinseokCHAE <minseok6739@naver.com>
2021-08-27 17:52:03 +0900

    update_0827_17:51

--- dacon/polymer_properties/polymer_properties_npy_load.py
+++ dacon/polymer_properties/polymer_properties_npy_load.py
@@ -32,7 +32,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout
 def create_deep_learning_model():
     model = Sequential()
-    model.add(Dense(2048, input_dim=2048, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(1024, input_dim=2048, kernel_initializer='normal', activation='relu'))
     model.add(Dense(1024, activation='relu'))
     model.add(Dense(512, activation='relu'))
     model.add(Dense(128, activation='relu'))
@@ -53,12 +53,12 @@ estimators = []
 # estimators.append(('standardize', StandardScaler()))
 estimators.append(('mlp', KerasRegressor(build_fn=create_deep_learning_model, epochs=21)))
 pipeline = Pipeline(estimators)
-kfold = KFold(n_splits=7)
+kfold = KFold(n_splits=4)
 results = cross_val_score(pipeline, X, Y, cv=kfold)
 # print("%.2f (%.2f) MAE" % (results.mean(), results.std()))
 
 model = create_deep_learning_model()
-model.fit(X, Y, epochs = 111)
+model.fit(X, Y, epochs = 21)
 test_y = model.predict(np_test_fps_array)
 ss['ST1_GAP(eV)'] = test_y
 ss.to_csv("../_data/dacon/polymer_properties/rdkit.csv", index=False)
