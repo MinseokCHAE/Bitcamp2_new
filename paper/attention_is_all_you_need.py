04fe6418e2ed732647c44647ef6f236ec33cf34d
MinseokCHAE <minseok6739@naver.com>
2021-09-13 17:42:54 +0900

    update_0913_17:42

--- paper/attention_is_all_you_need.py
+++ paper/attention_is_all_you_need.py
@@ -697,4 +697,5 @@ def calculate_bleu(data, src_field, trg_field, model, device, max_len = 50):
 bleu_score = calculate_bleu(test_data, SRC, TRG, model, device)
 
 print(f'BLEU score = {bleu_score*100:.2f}')
-    
+
+
