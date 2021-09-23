947120eae0c203753143c891f7073b8460fa03fb
MinseokCHAE <minseok6739@naver.com>
2021-09-23 11:29:30 +0900

    update_0923_11:29

--- keras02/keras74_autokeras.py
+++ keras02/keras74_autokeras.py
@@ -3,10 +3,29 @@ from tensorflow.keras.datasets import mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-model = ak.ImageClassifier(
-    overwrite=True,
-    max_trials=2
-)
+
+# ======== 1 ========
+# inputs = ak.ImageInput()
+# outputs = ak.ImageBlock(
+#     block_type='resnet',
+#     normalize=True,
+#     augment=False,
+# )(inputs)
+# outputs = ak.ClassificationHead()(outputs)
+
+# model = ak.AutoModel(inputs=inputs, outputs=outputs, overwrite=True, max_trials=1)
+
+
+# ======== 2 ========
+# model = ak.ImageClassifier(
+#     overwrite=True,
+#     max_trials=2
+# )
+
+
+# ======== 3 ========
+model = ak.StructuredDataClassifier(overwrite=True, max_trials=1) # classifier <=> regressor
+
 
 model.fit(x_train, y_train, epochs=5,)
 
