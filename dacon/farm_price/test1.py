b042e88e4588b114b4a4cc3c299c468757dc2999
MinseokCHAE <minseok6739@naver.com>
2021-09-28 09:50:03 +0900

    update_0928_09:49

--- dacon/farm_price/test1.py
+++ dacon/farm_price/test1.py
@@ -72,12 +72,14 @@ def at_nmae(pred, dataset):
 0.001 5 128 0.8 0.8 5 21 7 = 0.23015
 0.01 5 128 0.8 0.8 7 21 9 = 0.22934
 0.01 5 128 0.7 0.7 7 21 7 = 0.22652
-0.05 7 128 0.7 0.7 7 21 7 = 
+0.05 7 128 0.7 0.7 7 21 7 = 0.22831
+0.021 3 128 0.7 0.7 7 21 7 = 0.22995
+0.01 3 128 0.7 0.7 7 21 7 = 
 '''
 
 def model_train(x_train, y_train, x_valid, y_valid) :
-    params = {'learning_rate': 0.05, 
-              'max_depth': 7, 
+    params = {'learning_rate': 0.01, 
+              'max_depth': 3, 
               'boosting': 'gbdt', 
               'objective': 'regression',  
               'is_training_metric': True, 
